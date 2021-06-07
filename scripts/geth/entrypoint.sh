c4b263b33bdd594eb2598c4e36bfb1cd56739bf3
Yan Mingzhi <realraindust@gmail.com>
2021-06-07 11:14:54 +0800

    update entry point geth args

--- scripts/geth/entrypoint.sh
+++ scripts/geth/entrypoint.sh
@@ -15,14 +15,14 @@ exec geth \
     --unlock "0xff93B45308FD417dF303D6515aB04D9e89a750Ca","0x8e0a907331554AF72563Bd8D43051C2E64Be5d35","0x24962717f8fA5BA3b931bACaF9ac03924EB475a0","0x148FfB2074A9e59eD58142822b3eB3fcBffb0cd7","0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485" \
     --password /root/password.txt \
     --ws \
-    --wsport 8545 \
-    --wsorigins="*" \
-    --wsaddr 0.0.0.0 \
+    --ws.port 8545 \
+    --ws.origins="*" \
+    --ws.addr 0.0.0.0 \
     --rpc \
     --rpcport 8545 \
     --rpccorsdomain="*" \
     --rpcaddr 0.0.0.0 \
     --networkid 5 \
-    --targetgaslimit 8000000 \
+    --miner.gastarget 8000000 \
     --allow-insecure-unlock \
     --mine
